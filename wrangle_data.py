48d5d2f7caef1cf12c165da0502e812ea9b02f5d
Ogladr Kjarr <40721677+ogladr-kjarr@users.noreply.github.com>
2025-03-07 16:01:11 +0000

    Followed linter instructions

--- wrangle_data.py
+++ wrangle_data.py
@@ -2,11 +2,13 @@ from pathlib import Path
 import polars as pl
 from polars import DataFrame
 
+
 def get_csv_file_list(folder: str) -> list:
     p = Path(folder)
     generator = p.glob('*.csv')
     file_list = list(generator)
     return file_list
+
 
 def load_csv_mutate_concatenate(files: list) -> DataFrame | None:
     result = None
@@ -24,6 +26,7 @@ def load_csv_mutate_concatenate(files: list) -> DataFrame | None:
 
     return result
 
+
 def transform_timestamp(d: DataFrame) -> DataFrame:
     d = d.with_columns(pl.col('SHOUR').replace(24, 0))
     d = d.with_columns(
@@ -38,34 +41,46 @@ def transform_timestamp(d: DataFrame) -> DataFrame:
     )
     return d
 
+
 def remove_duplicates(d: DataFrame) -> DataFrame:
     duplicate = d.select(
-        pl.col('SITECODE'), 
-        pl.col("AWSNO"), 
-        pl.col("TIMESTAMP"), 
+        pl.col('SITECODE'),
+        pl.col("AWSNO"),
+        pl.col("TIMESTAMP"),
         pl.col("FIELDNAME")).is_duplicated()
     d = d.filter(~duplicate)
     return d
+
 
 def write_wide_csv(folder: str, file: str, d: DataFrame):
     p = Path(folder + file)
     d.write_csv(p)
 
+
 def long_to_wide(d: DataFrame) -> DataFrame:
-    d = d.pivot('FIELDNAME', index=['SITECODE', 'AWSNO','TIMESTAMP'], values='VALUE')
+    d = d.pivot('FIELDNAME',
+                index=['SITECODE', 'AWSNO', 'TIMESTAMP'],
+                values='VALUE')
     return d
+
 
 def read_wide_format(folder: str, file: str) -> DataFrame:
     d = pl.read_csv(folder + file, try_parse_dates=True)
     return d
 
-def wrangle_files(folder: str):
+
+def wrangle_to_wide_format(folder: str):
     file_location_list = get_csv_file_list(folder)
     d = load_csv_mutate_concatenate(file_location_list)
     if d is None:
         raise IOError("No files loaded")
-    else: 
+    else:
         write_wide_csv(folder, 'wide_file.csv', d)
 
+
+def wrangle_to_postgres(folder: str):
+    pass
+
+
 if __name__ == '__main__':
-    wrangle_files('/home/ogladr-kjarr/data/ceh_ecn/')
+    wrangle_to_wide_format('/home/ogladr-kjarr/data/ceh_ecn/')
