45875f9f0aa101a739e675c07ba4cada3577e0cd
Ogladr Kjarr <40721677+ogladr-kjarr@users.noreply.github.com>
2025-03-13 18:40:07 +0000

    Finished adding plots

--- visualise_data.py
+++ visualise_data.py
@@ -2,11 +2,12 @@ import polars as pl
 from polars import DataFrame
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def direct_matplot_lib(d: DataFrame):
 
-    d_filtered = d.filter(pl.col("SITECODE") == "T12")
+    d_filtered = d.filter(pl.col("SITECODE") == "T09")
 
     fig, ax = plt.subplots()
     ax.scatter(
@@ -15,13 +16,13 @@ def direct_matplot_lib(d: DataFrame):
         c=d_filtered["AWSNO"]
     )
 
-    ax.set_title('Dry Temperature at Cairngorms')
+    ax.set_title('Dry Temperature at Alice Holt')
     ax.set_xlabel('Year')
     ax.set_ylabel('Temperature / Celcius')
 
 
 def direct_seaborn_lib(d: DataFrame):
-    d_filtered = d.filter(pl.col("SITECODE") == "T12")
+    d_filtered = d.filter(pl.col("SITECODE") == "T09")
 
     fig, ax = plt.subplots()
     sns.scatterplot(
@@ -31,19 +32,28 @@ def direct_seaborn_lib(d: DataFrame):
         hue="AWSNO",
         ax=ax,
     )
-    ax.set_title('Dry Temperature at Cairngorms')
+    ax.set_title('Dry Temperature at Alice Holt')
     ax.set_xlabel('Year')
     ax.set_ylabel('Temperature / Celcius')
 
 
 def summarized_single_site(d: DataFrame):
-    d_filtered = d.filter(pl.col("SITECODE") == "T12")
+    d_filtered = d.filter(pl.col("SITECODE") == "T09")
     ts = d_filtered.select(pl.col('TIMESTAMP'), pl.col('DRYTMP'))
     sorted_ts = ts.sort('TIMESTAMP')
     average_ts = sorted_ts.group_by_dynamic(
         "TIMESTAMP", every="1mo").agg(pl.col("DRYTMP").mean())
 
-    sns.relplot(data=average_ts, kind='line', x='TIMESTAMP', y='DRYTMP')
+    fig, ax = plt.subplots()
+    sns.lineplot(
+        data=average_ts,
+        x='TIMESTAMP',
+        y='DRYTMP',
+        ax=ax)
+
+    ax.set_title('Monthly Dry Temperature at Alice Holt')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Temperature / Celcius')
 
 
 def summarized_multiple_sites(d: DataFrame):
@@ -64,12 +74,93 @@ def summarized_multiple_sites(d: DataFrame):
         ).alias('TIMESTAMP')
     ).select(
         pl.col('TIMESTAMP'),
+        pl.col('YEAR'),
         pl.col('SITECODE'),
         pl.col('MEANDRYTMP')
     ).sort('TIMESTAMP')
 
-    g = sns.FacetGrid(ts_grouped, col="SITECODE", col_wrap=2, height=6)
+    order = ts_grouped.select(pl.col('SITECODE')).to_series().to_list()
+    site_order = sorted(list(set(order)))
+
+    g = sns.FacetGrid(
+        ts_grouped,
+        col="SITECODE",
+        col_wrap=2,
+        height=6,
+        col_order=site_order)
+
     g.map(sns.pointplot, "TIMESTAMP", "MEANDRYTMP")
+    g.set_xlabels('Year')
+    g.set_ylabels('Temperature / Celcius')
+    g.set_titles('Dry Temp at site {col_name}')
+
+
+def one_year_one_site(d: DataFrame):
+
+    one_year = d.with_columns(
+        pl.col('TIMESTAMP').dt.year().alias('YEAR'),
+        pl.col('TIMESTAMP').dt.month().alias('MONTH')
+    ).filter((pl.col('YEAR') == 2015) & (pl.col('SITECODE') == 'T09'))
+
+    fig, ax = plt.subplots()
+    sns.kdeplot(
+        data=one_year, x="DRYTMP", hue="MONTH", palette="crest", ax=ax
+    )
+
+    ax.set_title('Dry Temperature at Alice Holt in 2015')
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Temperature / Celcius')
+
+
+def ridge(d: DataFrame):
+
+    # sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
+
+    # Create the data
+    ts_ridge = d.with_columns(
+        pl.col('TIMESTAMP').dt.year().alias('YEAR'),
+        pl.col('TIMESTAMP').dt.month().alias('MONTH')
+    ).filter(
+            (pl.col('SITECODE') == 'T09') &
+            (pl.col('YEAR') == 2000)
+    ).select(['MONTH', 'DRYTMP'])
+
+    x = ts_ridge.select(pl.col('DRYTMP')).to_series().to_list()
+    g = ts_ridge.select(pl.col('MONTH')).to_series().to_list()
+    df = pd.DataFrame(dict(x=x, g=g))
+    
+    # Initialize the FacetGrid object
+    pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
+    g = sns.FacetGrid(df, row="g", hue="g", aspect=15, height=.5, palette=pal)
+
+    # Draw the densities in a few steps
+    g.map(sns.kdeplot, "x",
+        bw_adjust=.5, clip_on=False,
+        fill=True, alpha=1, linewidth=1.5)
+    g.map(sns.kdeplot, "x", clip_on=False, color="w", lw=2, bw_adjust=.5)
+
+    # passing color=None to refline() uses the hue mapping
+    g.refline(y=0, linewidth=2, linestyle="-", color=None, clip_on=False)
+
+
+    # Define and use a simple function to label the plot in axes coordinates
+    def label(x, color, label):
+        ax = plt.gca()
+        ax.text(0, .2, label, fontweight="bold", color=color,
+                ha="left", va="center", transform=ax.transAxes)
+
+
+    g.map(label, "x")
+
+    # Set the subplots to overlap
+    g.figure.subplots_adjust(hspace=-.25)
+
+    # Remove axes details that don't play well with overlap
+    g.set_titles("")
+    g.set(yticks=[], ylabel="")
+    g.despine(bottom=True, left=True)
+    g.fig.suptitle('Dry Temperatures at Alice Holt in 2000 by Month')
+    g.set_axis_labels('Temperature / Celcius')
 
 
 if __name__ == '__main__':
@@ -80,3 +171,5 @@ if __name__ == '__main__':
     direct_seaborn_lib(d)
     summarized_single_site(d)
     summarized_multiple_sites(d)
+    one_year_one_site(d)
+    ridge(d)
